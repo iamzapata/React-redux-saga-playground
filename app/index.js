a917309a05f0910cdbe98219df294a3488660321
Andres Zapata <me@azapata.io>
2017-06-27 18:16:14 -0500

    Setup application store with saga

--- app/index.js
+++ app/index.js
@@ -1,7 +1,21 @@
+import "babel-polyfill"
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux'
 import Application from 'components/Application';
+import configureStore from './store/configureStore'
+
+const store = configureStore();
 
 const container = document.getElementById('app');
 
-render(React.createElement(Application), container);
+import rootSaga from './sagas';
+
+store.runSaga(rootSaga);
+
+render(
+  <Provider store={store}>
+    <Application />
+  </Provider>,
+  container
+)
